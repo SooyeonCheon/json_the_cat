b7f392693e07f0a8214753ebee53d2ccafd01841
SooyeonCheon123 <suyeon5445@gmail.com>
2023-02-16 17:29:31 -0800

    testing cats as a service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,23 @@
 const request = require("request");
-const breedName = process.argv[2];
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
-  (error, response, body) => {
-    if (error) {
-      return console.log("Failed to request details: ", error);
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        return console.log("Failed to request details: ", error);
+      }
+  
+      const data = JSON.parse(body);
+      const breed = data[0];
+  
+      if (breed) {
+        console.log(breed.description);
+      } else {
+        console.log(`${breedName} not found`);
+      }
     }
+  );
+};
 
-    const data = JSON.parse(body);
-    const breed = data[0];
-
-    if (breed) {
-      console.log(breed.description);
-    } else {
-      console.log(`${breedName} not found`);
-    }
-  }
-);
+module.exports = { fetchBreedDescription };
